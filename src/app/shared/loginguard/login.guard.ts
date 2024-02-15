3cb9d7741d1944173d35799766e2dd0545ed6962
DeepakFromZeus <manedeep2001@gmail.com>
2024-02-15 11:03:36 +0530

    switch to angular 16

--- src/app/shared/loginguard/login.guard.ts
+++ src/app/shared/loginguard/login.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginGuard implements CanActivate {
+export class LoginGuard  {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): boolean {
